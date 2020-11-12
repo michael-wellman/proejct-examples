29d6ab572339a207b4a7cb1f1ea7db50f8d101b9
Michael Wellman <michael@brasstackscollective.com>
2020-11-12 17:37:28 -0600

    updates to JS and HTML

--- animated-navigation/script.js
+++ animated-navigation/script.js
@@ -6,13 +6,6 @@ const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
 
-//Control Navigation Function
-function navAnimation(direction1, direction2) {
-    navItems.forEach((navItem, i) => {
-        navItem.classList.replace(`slide-${direction1}-${i}`, `slide-${direction2}-${i}`);
-    });
-}
-
 //Toggle Nav Function
 function toggleNav() {
     //Toggle: Menu Bars Open/Closed
@@ -22,7 +15,8 @@ function toggleNav() {
     overlay.classList.toggle('overlay-active');
     if(overlay.classList.contains('overlay-active')) {
         // Animate In
-        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+        overlay.classList.remove('overlay-slide-left');
+        overlay.classList.add('overlay-slide-right');
 
         //Animat In - Nav Items
         nav1.classList.remove('slide-out-1');
@@ -35,14 +29,10 @@ function toggleNav() {
         nav4.classList.add('slide-in-4');
         nav5.classList.remove('slide-out-5');
         nav5.classList.add('slide-in-5');
-
-        const navItems = [nav1, nav2, nav3, nav4, nav5];
-
-        navAnimation('out', 'in');
-
     } else {
         // Animate Out
-        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+        overlay.classList.remove('overlay-slide-right');
+        overlay.classList.add('overlay-slide-left')
 
         //Animat Out - Nav Items
         nav1.classList.remove('slide-in-1');
@@ -55,8 +45,6 @@ function toggleNav() {
         nav4.classList.add('slide-out-4');
         nav5.classList.remove('slide-in-5');
         nav5.classList.add('slide-out-5');
-
-        navAnimation('in', 'out');
     }
 }
 
